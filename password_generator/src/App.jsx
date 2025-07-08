2e2ca9d993aa344aa0c650b06b1324d048b68d9b
shruti-40619 <thakurshruti40619@gmail.com>
2025-07-08 22:51:34 +0530

    initial commit

---
+++ password_generator/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  
+  return (
+    <div>
+      <div className='flex-col max-w-5xl bg-gray-700 mt-7 ml-20 rounded-2xl'>
+        <div>
+         <h1 className='text-2xl font-bold text-white text-center'>Password Generator</h1>
+        </div>
+        <div>
+          
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
